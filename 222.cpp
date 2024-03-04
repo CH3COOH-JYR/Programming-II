e4f6d3ca305d5fa5f3d7626c6f7ccdef1fe0990f
Jyrqwq <1241516038@qq.com>
2024-03-04 16:43:40 +0800

    class-try

--- 222.cpp
+++ 222.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <cstdio>
 using namespace std;
+class clock;
+class clock
+{
+public:
+    int hour;
+    int minute;
+    int second;
+    void settime(int h, int m, int s);
+    void showtime();
+};
+void clock::settime(int h, int m, int s)
+{
+    hour = h;
+    minute = m;
+    second = s;
+}
+void clock::showtime()
+{
+    cout << hour << ":" << minute << ":" << second << endl;
+}
 int main()
 {
+    class clock myclock;
+    myclock.settime(8, 30, 30);
+    myclock.showtime();
+    return 0;
 }
