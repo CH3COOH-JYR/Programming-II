40400daf3395a15d16ca93aa8cfc084626952963
Jyrqwq <1241516038@qq.com>
2024-03-04 17:17:17 +0800

    class is difficult

--- 111.cpp
+++ 111.cpp
@@ -1,1 +1,16 @@
 #include <iostream>
+#include <cstdio>
+using namespace std;
+class a
+{
+public:
+    static int aa;
+    int bb;
+};
+int a::aa = 10;
+int main()
+{
+    class a c;
+    cout << c.aa << endl;
+    return 0;
+}
